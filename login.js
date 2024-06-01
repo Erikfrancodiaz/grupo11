28c5673f99531b1d3a42720e1b310ff406fdaf1a
erik diaz <erikfrancodiaz@gmail.com>
2024-05-31 22:01:22 -0300

    upgrade

---
+++ login.js
@@ -0,0 +1,16 @@
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault(); 
+
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+    const errorMessage = document.getElementById('error-message');
+
+    if (username === '' || password === '') {
+        errorMessage.textContent = 'Por favor, complete todos los campos.';
+    } else {
+        errorMessage.textContent = '';
+        
+        window.location.href = 'sesion.html'; 
+    }tm
+});
+
